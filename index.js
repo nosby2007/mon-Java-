13fd673f037f74ce87923494e03d904ada55abb3
nosby2007 <107397519+nosby2007@users.noreply.github.com>
2022-09-22 00:13:41 +0400

    2 files modified

---
+++ index.js
@@ -0,0 +1,13 @@
+let countEl = document.getElementById("count-el")
+let count = 5
+ function increment() {
+   count  = count + 1
+   countEl.innerText = count
+ }
+ increment ()
+
+ //save button, function save
+functionn save() {
+  
+}
+
